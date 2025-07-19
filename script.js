f0f96cda0cf631c581a2937af40621600d621ffa
Tran Nghia <tplnghia2416@apcs.fitus.edu.vn>
2025-07-19 20:30:41 +0700

    DownloadButton

--- script.js
+++ script.js
@@ -13,3 +13,38 @@ document.addEventListener('click', function(e) {
     }
   }
 });
+
+// Download button functionality
+const downloadButton = document.querySelector('.downloadButton');
+const spinner = document.querySelector('.spinner');
+const errorMessage = document.querySelector('.errorMessage');
+const url = downloadButton.getAttribute('data-url');
+
+downloadButton.addEventListener('click', async e => {
+  e.preventDefault();
+  errorMessage.classList.add('hidden');
+  spinner.classList.remove('hidden');
+
+  // Simulate a delay to show the spinner
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
+  try {
+    // Throw an error if the URL is not valid
+    const head = await fetch(url, { method: 'HEAD' });
+    if (!head.ok) throw new Error('File not found');
+
+    // Activate download
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = '';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  } catch(error) {
+    errorMessage.textContent = "Không tải được file. Vui lòng thử lại sau!";
+    errorMessage.classList.remove('hidden');
+    setTimeout(() => errorMessage.classList.add('hidden'), 2000);
+  } finally {
+    spinner.classList.add('hidden');
+  }
+})
